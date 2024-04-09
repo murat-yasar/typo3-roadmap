c9dc2b48fd483d76f1ba1a7a30754985b404fb3f
Murat Yaşar <murat.yasar@comkom.de>
2024-04-09 17:38:36 +0200

    Add navbar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function openRoad(evt, roadName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(roadName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("navbarTop");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
