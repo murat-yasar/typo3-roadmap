5b0362e32d349a30bd4957cf748c20ccab017c14
Murat Yaşar <murat.yasar@comkom.de>
2024-04-09 19:40:55 +0200

    format structure

--- js/script.js
+++ js/script.js
@@ -21,3 +21,14 @@ function navbarCollapse() {
     x.className = "topnav";
   }
 } 
+
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+} 
